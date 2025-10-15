9daeacb775351515ea0ca2ead6e0f078dea9075d
MVSSaiAditya2055 <2400032055@kluniversity.in>
2025-10-15 11:41:44 +0530

    first commit

---
+++ ECOM-FEnd/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import AboutUs from "./components/AboutUs";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+function APP(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/aboutus" element={<AboutUs/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default APP;
